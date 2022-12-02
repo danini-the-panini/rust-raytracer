815e8bac408040a6bf59e2a543b9c9895aa45cb6
Danielle Smith <danini@hey.com>
2022-12-02 08:49:29 +0200

    add surface normals

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,24 @@ use crate::vec3::{Vec3, Point3, Color, unit_vector, dot};
 use crate::color::write_color;
 use crate::ray::Ray;
 
-fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
+fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
   let oc = r.origin() - *center;
   let a = dot(&r.direction(), &r.direction());
   let b = 2.0 * dot(&oc, &r.direction());
   let c = dot(&oc, &oc) - radius*radius;
   let discriminant = b*b - 4.0*a*c;
-  discriminant > 0.0
+  if discriminant < 0.0 {
+    -1.0
+  } else {
+    (-b - discriminant.sqrt()) / (2.0*a)
+  }
 }
 
 fn ray_color(r: &Ray) -> Color {
-  if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-    return Color::new(1.0, 0.0, 0.0);
+  let t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
+  if t > 0.0 {
+    let N = unit_vector(r.at(t) - Vec3::new(0.0,0.0,-1.0));
+    return 0.5*Color::new(N.x()+1.0, N.y()+1.0, N.z()+1.0);
   }
   let unit_direction = unit_vector(r.direction());
   let t = 0.5*(unit_direction.y() + 1.0);
