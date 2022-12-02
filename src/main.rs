36a70d0338bf644a692365abc9fec68de2a2abd5
Danielle Smith <danini@hey.com>
2022-12-02 08:42:16 +0200

    add ray

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,46 @@
 mod vec3;
 mod color;
-use crate::vec3::{Vec3, Color};
+mod ray;
+
+use crate::vec3::{Vec3, Point3, Color, unit_vector};
 use crate::color::write_color;
+use crate::ray::Ray;
+
+fn ray_color(r: &Ray) -> Color {
+  let unit_direction = unit_vector(r.direction());
+  let t = 0.5*(unit_direction.y() + 1.0);
+  (1.0-t)*Color::new(1.0, 1.0, 1.0) + t*Color::new(0.5, 0.7, 1.0)
+}
 
 fn main() {
   // Image
 
-  let image_width = 256;
-  let image_height = 256;
+  let aspect_ratio = 16.0 / 9.0;
+  let image_width = 400;
+  let image_height = (image_width as f64 / aspect_ratio) as i32;
+
+  // Camera
+
+  let viewport_height = 2.0;
+  let viewport_width = aspect_ratio * viewport_height;
+  let focal_length = 1.0;
+
+  let origin = Point3::new(0.0, 0.0, 0.0);
+  let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
+  let vertical = Vec3::new(0.0, viewport_height, 0.0);
+  let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
 
   //Render
+
   println!("P3\n{image_width} {image_height}\n255");
 
   for j in (0..image_height).rev() {
     eprint!("\rScanlines remaining: {j} ");
     for i in 0..image_width {
-      let pixel_color = Color::new(
-        (i as f64) / (image_width as f64 - 1.0),
-        (j as f64) / (image_height as f64 - 1.0),
-        0.25
-      );
+      let u = i as f64 / (image_width as f64 - 1.0);
+      let v = j as f64 / (image_height as f64 - 1.0);
+      let r = Ray::new(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+      let pixel_color = ray_color(&r);
       write_color(pixel_color);
     }
   }
---
+++ src/ray.rs
@@ -0,0 +1,19 @@
+use crate::vec3::{Vec3, Point3};
+
+pub struct Ray {
+  dir: Vec3,
+  orig: Point3
+}
+
+impl Ray {
+  pub fn new(origin: Point3, direction: Vec3) -> Self {
+    Ray { orig: origin, dir: direction }
+  }
+
+  pub fn direction(&self) -> Point3 { self.dir }
+  pub fn origin(&self) -> Point3 { self.orig }
+
+  pub fn at(&self, t: f64) -> Point3 {
+    self.orig + t*self.dir
+  }
+}
