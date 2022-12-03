a650e17adb222dd53ea404100535c5aaa6b8dba3
Danielle Smith <danini@hey.com>
2022-12-03 13:54:03 +0200

    add interesting scene

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ mod material;
 use std::f64::INFINITY;
 use std::rc::Rc;
 
+use util::random_double_in_range;
+
 use crate::camera::Camera;
 use crate::hittable::Hittable;
 use crate::hittable_list::HittableList;
@@ -37,37 +39,69 @@ fn ray_color(r: &Ray, world: &dyn Hittable, depth: i32) -> Color {
   (1.0-t)*Color::new(1.0, 1.0, 1.0) + t*Color::new(0.5, 0.7, 1.0)
 }
 
+fn random_scene() -> HittableList {
+  let mut world = HittableList::new();
+
+  let ground_material  = Lambertian { albedo: Color::new(0.5, 0.5, 0.5) };
+  world.add(Box::new(Sphere { center: Point3::new(0.0,-1000.0,0.0), radius: 1000.0, material: Rc::new(ground_material) }));
+
+  for a in -11..11 {
+    for b in -11..11 {
+      let choose_mat = random_double();
+      let center = Point3::new(a as f64 + 0.9*random_double(), 0.2, b as f64 + 0.9*random_double());
+
+      if (center - Point3::new(4.0, 0.2, 0.0)).length() > 0.9 {
+        if choose_mat < 0.8 {
+          // diffuse
+          let albedo = Color::random() * Color::random();
+          let material = Rc::new(Lambertian { albedo });
+          world.add(Box::new(Sphere { center, radius: 0.2, material }));
+        } else if choose_mat < 0.95 {
+          // metal
+          let albedo = Color::random_in_range(0.5, 1.0);
+          let fuzz = random_double_in_range(0.0, 0.5);
+          let material = Rc::new(Metal { albedo, fuzz });
+          world.add(Box::new(Sphere { center, radius: 0.2, material }));
+        } else {
+          // glass
+          let material = Rc::new(Dialectric { ir: 1.5 });
+          world.add(Box::new(Sphere { center, radius: 0.2, material }));
+        }
+      }
+    }
+  }
+
+  let material1 = Rc::new(Dialectric { ir: 1.5 });
+  world.add(Box::new(Sphere { center: Point3::new(0.0, 1.0, 0.0), radius: 1.0, material: material1 }));
+
+  let material2 = Rc::new(Lambertian { albedo: Color::new(0.4, 0.2, 0.1) });
+  world.add(Box::new(Sphere { center: Point3::new(-4.0, 1.0, 0.0), radius: 1.0, material: material2 }));
+
+  let material3 = Rc::new(Metal { albedo: Color::new(0.7, 0.6, 0.5), fuzz: 0.0 });
+  world.add(Box::new(Sphere { center: Point3::new(4.0, 1.0, 0.0), radius: 1.0, material: material3 }));
+
+  world
+}
+
 fn main() {
   // Image
 
-  let aspect_ratio = 16.0 / 9.0;
-  let image_width = 400;
+  let aspect_ratio = 3.0 / 2.0;
+  let image_width = 600;
   let image_height = (image_width as f64 / aspect_ratio) as i32;
-  let samples_per_pixel = 100;
+  let samples_per_pixel = 10;
   let max_depth = 50;
 
   // World
 
-  let mut world = HittableList::new();
-
-  let material_ground = Lambertian { albedo: Color::new(0.8, 0.8, 0.0) };
-  let material_center = Lambertian { albedo: Color::new(0.1, 0.2, 0.5) };
-  let material_left = Dialectric { ir: 1.5 };
-  let material_right = Metal { albedo: Color::new(0.8, 0.6, 0.2), fuzz: 0.0 };
-
-  world.add(Box::new(Sphere { center: Point3::new(0.0,-100.5,-1.0), radius: 100.0, material: Rc::new(material_ground) }));
-  world.add(Box::new(Sphere { center: Point3::new( 0.0, 0.0, -1.0), radius: 0.5, material: Rc::new(material_center) }));
-  world.add(Box::new(Sphere { center: Point3::new(-1.0, 0.0, -1.0), radius: 0.5, material: Rc::new(material_left) }));
-  world.add(Box::new(Sphere { center: Point3::new(-1.0, 0.0, -1.0), radius: -0.45, material: Rc::new(material_left) }));
-  world.add(Box::new(Sphere { center: Point3::new( 1.0, 0.0, -1.0), radius: 0.5, material: Rc::new(material_right) }));
-
+  let world = random_scene();
 
   // Camera
-  let lookfrom = Point3::new(3.0,3.0,2.0);
-  let lookat = Point3::new(0.0,0.0,-1.0);
+  let lookfrom = Point3::new(12.0, 2.0, 3.0);
+  let lookat = Point3::new(0.0,0.0,0.0);
   let vup = Vec3::new(0.0,1.0,0.0);
-  let dist_to_focus = (lookfrom-lookat).length();
-  let aperture = 2.0;
+  let dist_to_focus = 10.0;
+  let aperture = 0.1;
   
   let cam = Camera::new(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, dist_to_focus);
 
